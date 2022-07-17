c468efdf74a90baa6b134fdf4484e4468cef33b3
Bharat Pandey <bharat@jivrus.com>
2022-07-17 17:28:35 +0530

    Introduced Auth service

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,23 @@
+import 'package:notes/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+
+  // login function
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+
+  // create user function
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+
+  // logout function
+  Future<void> logOut();
+
+  // send email verification function
+  Future<void> sendEmailVerification();
+}
